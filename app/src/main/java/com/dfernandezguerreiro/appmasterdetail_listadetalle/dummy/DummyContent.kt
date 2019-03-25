e8ab6faa90070bff65bb44c091a2c5205c6b845b
DavidFernandezGuerreiro <davidferguer99@gmail.com>
2019-03-25 17:10:34 +0100

    finalizado

---
+++ app/src/main/java/com/dfernandezguerreiro/appmasterdetail_listadetalle/dummy/DummyContent.kt
@@ -0,0 +1,62 @@
+package com.dfernandezguerreiro.appmasterdetail_listadetalle.dummy
+
+import java.util.ArrayList
+import java.util.HashMap
+
+/**
+ * Helper class for providing sample content for user interfaces created by
+ * Android template wizards.
+ *
+ * TODO: Replace all uses of this class before publishing your app.
+ *
+ * https://kotlinlang.org/docs/reference/object-declarations.html#object-declarations
+ * Esta es la manera de kotlin de declarar un singleton
+ *
+ */
+object DummyContent {
+
+    /**
+     * El array de los posts.
+     */
+    val ITEMS: MutableList<DummyItem> = ArrayList()
+
+    /**
+     * Un hash map para los posts identificado por los IDs
+     * ¿Que es un HashMap? https://www.todoexpertos.com/categorias/tecnologia-e-internet/programacion/java/respuestas/y15actdgfmyx6/explicacion-de-como-se-utiliza-hashmap
+     */
+    val ITEM_MAP: MutableMap<String, DummyItem> = HashMap()
+
+    //private val COUNT = 25
+    // ya no hace falta
+    // era para construir la lista de items
+    // private val COUNT = 25
+
+    /**
+     * inicialización del singleton
+     */
+    init {
+        // En este paso necesitamos añadir los posts.
+        // TODO hacer la peticion doAsync, traer el json y parsearlo
+        for (i in 1..COUNT) {
+            addItem(DummyItem(idPost,tituloPost,resumenPost))
+        }
+    }
+
+    /**
+     * Añadimos los posts al arraylist y los metemos en el hashmap tambien
+     * @param item objeto con el contenido del post
+     */
+    private fun addItem(item: DummyItem) {
+        ITEMS.add(item)
+        ITEM_MAP.put(item.id, item)
+    }
+
+    /**
+     * Esta clase la utilizamos para almacenar los posts
+     * En el constructor le pasamos el id, titulo y resumen
+     */
+    data class DummyItem(val id: String, val title: String, val resumen: String) {
+        // personalizamos toString para que nos devuelva el titulo
+        override fun toString(): String = title
+    }
+}
